0890572f7dc8d2469a504d2a5e5491fc7d512a7e
Lakshay14ru <lakshaykundariyayt@gmail.com>
2026-06-09 11:54:09 +0530

    Day 2: implemented contract cleaning and validation pipeline

--- src/ingestion/file_processor.py
+++ src/ingestion/file_processor.py
@@ -1,13 +1,17 @@
 from src.ingestion.cleaner import TextCleaner
 from src.ingestion.schema import ContractSchema
+from src.ingestion.validator import ContractValidator
 
 
 class ContractProcessor:
 
     def __init__(self):
         self.cleaner = TextCleaner()
+        self.validator = ContractValidator()
 
     def process_text(self, file_name: str, text: str):
+
+        self.validator.validate(file_name, text)
 
         cleaned_text = self.cleaner.clean(text)
 
